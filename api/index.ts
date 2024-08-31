e2e0cd03ba14d407d11c7648a481e932daa04daa
C0D3O <1111eugene.eugene@gmail.com>
2024-08-31 17:46:57 +0300

    upd

--- api/index.ts
+++ api/index.ts
@@ -16,14 +16,24 @@ app.post('/estimate', async (req: Request, res: Response) => {
 	if (!supportedTokens.includes(inputCurrency) || !supportedTokens.includes(outputCurrency)) res.status(400);
 
 	const fetchedPrices = await prices.updateAmount(inputAmount, inputCurrency, outputCurrency);
+	console.log(fetchedPrices);
 
-	res.send('Hello World');
+	if (inputCurrency === 'USDT' || inputCurrency === 'ETH') {
+		const [maxExchange, maxPrice] = Object.entries(fetchedPrices).reduce((max, current) => (current[1] > max[1] ? current : max));
+
+		res.send({ name: maxExchange, price: maxPrice });
+	} else {
+		const [minExchange, minPrice] = Object.entries(fetchedPrices).reduce((min, current) => (current[1] < min[1] ? current : min));
+
+		res.send({ exchange: minExchange, price: minPrice });
+	}
 });
 
 app.post('/getRates', async (req: Request, res: Response) => {
 	const { inputCurrency, outputCurrency } = req.body;
 
 	if (!inputCurrency || !outputCurrency) res.status(400);
+
 	const fetchedPrices = await prices.updateAmount(1, inputCurrency, outputCurrency);
 
 	res.send(fetchedPrices);
