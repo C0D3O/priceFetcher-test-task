0f7e55d112f12df330ba3b186edd6c8b0b937280
C0D3O <1111eugene.eugene@gmail.com>
2024-08-31 18:06:23 +0300

    upd

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from 'express';
-
+import express, { NextFunction, Request, Response } from 'express';
+import { check, validationResult } from 'express-validator';
 import { wsClient } from '../lib/wsClient';
 import { supportedTokens } from '../lib/evm';
 
@@ -9,34 +9,58 @@ const prices = new wsClient();
 
 const app = express();
 
-app.post('/estimate', async (req: Request, res: Response) => {
-	const { inputAmount, inputCurrency, outputCurrency } = req.body;
+const validateReq = [
+	check('inputCurrency').isString().withMessage('inputCurrency must be a string'),
+	check('outputCurrency').isString().withMessage('outputCurrency must be a string'),
+	check('inputAmount').optional().isNumeric().withMessage('inputAmount must be a number').toFloat(),
+	(req: Request, res: Response, next: NextFunction) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-	if (!inputAmount || !inputCurrency || !outputCurrency) res.status(400);
-	if (!supportedTokens.includes(inputCurrency) || !supportedTokens.includes(outputCurrency)) res.status(400);
+		const { inputCurrency, outputCurrency } = req.body;
 
-	const fetchedPrices = await prices.updateAmount(inputAmount, inputCurrency, outputCurrency);
-	console.log(fetchedPrices);
+		if (!supportedTokens.includes(inputCurrency)) {
+			return res.status(400).json({ errors: [{ msg: 'Unsupported currency', param: 'inputCurrency' }] });
+		} else if (!supportedTokens.includes(outputCurrency)) {
+			return res.status(400).json({ errors: [{ msg: 'Unsupported currency', param: 'outputCurrency' }] });
+		}
 
-	if (inputCurrency === 'USDT' || inputCurrency === 'ETH') {
-		const [maxExchange, maxPrice] = Object.entries(fetchedPrices).reduce((max, current) => (current[1] > max[1] ? current : max));
+		next();
+	},
+];
 
-		res.send({ name: maxExchange, price: maxPrice });
-	} else {
-		const [minExchange, minPrice] = Object.entries(fetchedPrices).reduce((min, current) => (current[1] < min[1] ? current : min));
+app.post('/estimate', validateReq, async (req: Request, res: Response) => {
+	try {
+		const { inputAmount, inputCurrency, outputCurrency } = req.body;
 
-		res.send({ exchange: minExchange, price: minPrice });
+		const fetchedPrices = await prices.updateAmount(inputAmount, inputCurrency, outputCurrency);
+
+		if (inputCurrency === 'USDT' || inputCurrency === 'ETH') {
+			const [maxExchange, maxPrice] = Object.entries(fetchedPrices).reduce((max, current) => (current[1] > max[1] ? current : max));
+
+			res.send({ name: maxExchange, price: maxPrice });
+		} else {
+			const [minExchange, minPrice] = Object.entries(fetchedPrices).reduce((min, current) => (current[1] < min[1] ? current : min));
+
+			res.send({ exchange: minExchange, price: minPrice });
+		}
+	} catch (error: any) {
+		res.send({ error: `Internal server error: \n${error.message}` }).status(500);
 	}
 });
 
-app.post('/getRates', async (req: Request, res: Response) => {
-	const { inputCurrency, outputCurrency } = req.body;
+app.post('/getRates', validateReq, async (req: Request, res: Response) => {
+	try {
+		const { inputCurrency, outputCurrency } = req.body;
 
-	if (!inputCurrency || !outputCurrency) res.status(400);
+		const fetchedPrices = await prices.updateAmount(1, inputCurrency, outputCurrency);
 
-	const fetchedPrices = await prices.updateAmount(1, inputCurrency, outputCurrency);
-
-	res.send(fetchedPrices);
+		res.send(fetchedPrices);
+	} catch (error: any) {
+		res.send({ error: `Internal server error: \n${error.message}` }).status(500);
+	}
 });
 
 app.listen(3000);
